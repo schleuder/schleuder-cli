060a61cffda630c8113a1b354cd94c1fa3354a24
paz <paz@nadir.org>
2016-12-09 13:43:57 +0100

    Test list-options

--- spec/schleuder-cli/cli_spec.rb
+++ spec/schleuder-cli/cli_spec.rb
@@ -61,6 +61,11 @@ describe SchleuderCli do
     expect(output).to eq('No such option')
   end
 
+  it "lists list-options" do
+    output = run_cli(%w[lists list-options])
+    expect(output).to eq("bounces_drop_all\nbounces_drop_on_headers\nbounces_notify_admins\nforward_all_incoming_to_admins\nheaders_to_meta\ninclude_list_headers\ninclude_openpgp_header\nkeep_msgid\nkeywords_admin_notify\nkeywords_admin_only\nlanguage\nlog_level\nlogfiles_to_keep\nmax_message_size_kb\nopenpgp_header_preference\npublic_footer\nreceive_admin_only\nreceive_authenticated_only\nreceive_encrypted_only\nreceive_from_subscribed_emailaddresses_only\nreceive_signed_only\nsend_encrypted_only\nsubject_prefix\nsubject_prefix_in\nsubject_prefix_out")
+  end
+
   it "lists keys for existing list" do
     output = run_cli(%w[keys list something@localhost])
     expect(output).to eq("52507B0163A8D9F0094FFE03B1A36F08069E55DE paz@nadir.org\nFE1671DA071B08023A260CC2D2915EC47415257A something-request@localhost")
